9e0594a67944547fdd20403f84c9c433c041fd78
Dusan Peric <dusan.peric@page.ba>
2025-11-06 20:54:28 +0100

    stack navigator

---
+++ src/navigation/StackNavigator.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/Home";
+import About from "../screens/About";
+
+const Stack = createStackNavigator();
+
+const screenOptionStyle = {
+  headerStyle: {
+    backgroundColor: "#FF6347",
+  },
+  headerTintColor: "white",
+};
+
+const MainStackNavigator = ({ navigation }) => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="About" component={About} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainStackNavigator;
+
+// Easy:
+
+// Create a new screen called "Contact.js" in the screens folder.
+// This screen should contain a single Text element with the words "Contact Page".
+// Add this screen to your MainStackNavigator and test that you can navigate to
+// it from the Home screen.
---
+++ src/screens/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const About = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to About Screen</Text>
+      <Button title="Go to Home Screen" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default About;
